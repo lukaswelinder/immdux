90b4c032da148c555224225c13db16d8b7e7bfa3
Lukas Welinder <lukas@welinder.co>
2019-12-15 11:44:50 -0800

    refactor: simplify dispatch handlers

--- packages/immdux-core/src/handlers/store.ts
+++ packages/immdux-core/src/handlers/store.ts
@@ -8,8 +8,8 @@ import { SET_STATE } from '../actions/actionTypes';
 
 import { AnyAction, Dispatch } from '../types';
 
-// TODO: support scoped dispatch
-// TODO: support dispatching multiple actions
+// TODO: improve typing approach for action/dispatch/middleware
+
 /**
  * Dispatches action to reducers.
  */
@@ -25,46 +25,32 @@ export function dispatch<A = any>(action: A): A {
  * This should ideally be done before reducers are registered, but is not required.
  */
 export function setState(state: any) {
-  if (isDispatching) throw new Error('Setting state while reducer is executing is forbidden.');
   const prev = struct.current;
   const next = fromJS(state);
-  setIsDispatching(true);
-  const action = { type: SET_STATE, payload: { prev, next } };
-  executeReducers(next, action);
-  setIsDispatching(false);
-  flushQueuedObservers(action);
-  return next;
+  struct.current = next;
+  return dispatch({ type: SET_STATE, payload: { prev, next } });
 }
 
 /** @hidden */
 export function dispatchInternal<A extends AnyAction = AnyAction>(action: A): A {
   setIsDispatching(true);
-  executeReducers(struct.current, action);
+  const inputState = struct.current;
+  // Will not trigger observers if inputState is unchanged.
+  struct.update(() => inputState);
+  // Iterate over branch reducers (depth first in order registered).
+  for (const [path, reducer] of reducers._cachedIterable)
+    struct.updateIn(path, (branchState: any) => {
+      return reducer(branchState, action);
+    });
   setIsDispatching(false);
   flushQueuedObservers(action);
   return action;
 }
 
-// TODO: improve typing approach
 /** @hidden */
 export let dispatchThroughMiddleware: any = dispatchInternal;
+
 /** @hidden */
 export function setDispatchThroughMiddleware<D = Dispatch<any>>(d: D) {
   dispatchThroughMiddleware = d;
-}
-
-/**
- * Calls registered reducers to update state.
- * @hidden
- */
-function executeReducers(inputState: any, action: AnyAction) {
-  // Will not trigger observers if inputState is unchanged.
-  struct.update(() => inputState);
-  // Iterate over branch reducers (depth first is only order guarantee).
-  for (const [path, reducer] of reducers._cachedIterable)
-    struct.updateIn(path, (branchState: any) => {
-      return reducer(branchState, action);
-    });
-  // Returns current immutable state.
-  return struct.current;
 }
